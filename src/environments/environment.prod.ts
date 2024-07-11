80ed1eeb60537a779ddea6cc79b540eeca7e1c06
MUKESHYADAV833 <70187667+MUKESHYADAV833@users.noreply.github.com>
2024-07-11 21:39:55 +0530

    Final commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: true,
+    encryptionKey: '336ggvrfresxcknhmmjgt6vce'
+
+  };
+  
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+
+export const environment = {
+    production: false,
+    encryptionKey: '336ggvrfresxcknhmmjgt6vce'
+  };
